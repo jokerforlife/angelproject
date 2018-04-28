3bd028d671a57867a3d5606bb3c4fcb0b9a94963
unknown <fifarockafif@gmail.com>
2018-04-27 17:09:41 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,30 @@
+/* var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+}); */
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+
+/*app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});*/
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
